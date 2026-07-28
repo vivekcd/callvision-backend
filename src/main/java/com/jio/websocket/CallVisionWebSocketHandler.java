2568a7f44b7389325c994b93788ef46b45d885f9
vivekcd6 <vivekcd6@gmail.com>
2026-07-28 15:16:19 +0530

    Added WebSocket logging for debugging

--- src/main/java/com/jio/websocket/CallVisionWebSocketHandler.java
+++ src/main/java/com/jio/websocket/CallVisionWebSocketHandler.java
@@ -1,5 +1,7 @@
 package com.jio.callvision.websocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -8,14 +10,22 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class CallVisionWebSocketHandler extends TextWebSocketHandler {
     
+    private static final Logger logger = LoggerFactory.getLogger(CallVisionWebSocketHandler.class);
+    
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        logger.info("🔗 WebSocket connected! Session ID: {}", session.getId());
+        session.sendMessage(new TextMessage("WebSocket Connected"));
+    }
+    
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        // Echo the message back to client
+        logger.info("📩 Received message: {}", message.getPayload());
         session.sendMessage(new TextMessage("Connected: " + message.getPayload()));
     }
     
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        session.sendMessage(new TextMessage("WebSocket Connected"));
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        logger.info("🔌 WebSocket disconnected! Session ID: {}, Status: {}", session.getId(), status);
     }
 }
